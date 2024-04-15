1a4e987ee584ac84c94010f658b06fd4584dda6b
goibnda <gobindabauri008@gmail.com>
2024-04-15 13:28:08 +0530

    edit in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Landing from "./components/Landing"; 
-import Login from "./components/Login";
-import SignUp from "./components/SignUp";
-import Profile from "./components/Profile"
+// import Login from "./components/Login";
+// import SignUp from "./components/SignUp";
+// import Profile from "./components/Profile"
 
 function App() {
   return (
